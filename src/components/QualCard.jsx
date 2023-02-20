71628a972367ae62506d5e2b02a865e81feac9d1
Do Hyun Nam <dollis2002@gmail.com>
2023-02-19 21:12:30 -0600

    Ficed

--- src/components/QualCard.jsx
+++ src/components/QualCard.jsx
@@ -2,15 +2,13 @@ import React from "react";
 
 export default function QualCard({ image, title, desc, date }) {
   return (
-    <div className="flex flex-row justify-start items-center w-full h-max rounded-xl p-2 mb-4 shadow-2xl">
-      <div className="grid grid-cols-4">
-        <div className="col-span-1 p-2 justify-center items-center">
-          <img src={image} alt="qual-logo" className="max-h-20 mx-auto" />
-        </div>
-        <div className="col-span-3 flex flex-col p-2">
-          <h1 className="font-title text-2xl">{title}</h1>
-          <p className="">{date}</p>
-        </div>
+    <div className="grid grid-cols-4 w-full h-max rounded-xl p-2 mb-4 shadow-2xl">
+      <div className="col-span-3 flex flex-col p-2">
+        <h1 className="font-title text-2xl">{title}</h1>
+        <p className="">{date}</p>
+      </div>
+      <div className="col-span-1 p-2 justify-center items-center">
+        <img src={image} alt="qual-logo" className="max-h-20 mx-auto" />
       </div>
     </div>
   );
