abb81f299f5daf73619be69fd6dcf14bae751b0b
Do Hyun Nam <dollis2002@gmail.com>
2023-05-10 08:42:36 -0600

    it works?

--- src/components/QualCard.jsx
+++ src/components/QualCard.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 
 export default function QualCard({ image, title, desc, date }) {
+  const imageUrl = new URL(image, import.meta.url).href;
+
   return (
     <div className="grid grid-cols-4 w-full h-max rounded-xl p-2 mb-4 shadow-2xl">
       <div className="col-span-3 flex flex-col p-2">
@@ -8,7 +10,7 @@ export default function QualCard({ image, title, desc, date }) {
         <p className="">{date}</p>
       </div>
       <div className="col-span-1 p-2 justify-center items-center">
-        <img src={image} alt="qual-logo" className="max-h-20 mx-auto" />
+        <img src={imageUrl} alt="qual-logo" className="max-h-20 mx-auto" />
       </div>
     </div>
   );
